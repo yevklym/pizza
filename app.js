389e37ec359ea43fa374bf391b7229d7bfc45164
Yevhenii Klymenko <75336264+yevklym@users.noreply.github.com>
2020-12-11 10:06:35 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const expressHbs = require("express-handlebars");
+const hbs = require("hbs");
+const app = express();
+
+
+// set up layout
+app.engine("hbs", expressHbs(
+    {
+        layoutsDir: "views/layouts",
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use(express.static('public'))
+
+
+app.use("/", function(request, response){
+
+    response.render("home.hbs");
+});
+app.listen(3000);
