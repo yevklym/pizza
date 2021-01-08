92946844665478809e52dbc8ecc0706b8bf15f94
larbito31 <boukhobza.larbi31@gmail.com>
2021-01-08 17:21:39 +0100

    kontakt + ueberuns fixed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
 const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, 'public');
 
 
 // set up layout
@@ -18,8 +20,26 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static('public'))
 
 
-app.use("/", function(request, response){
+app.get("/", function(request, response){
 
     response.render("home.hbs");
 });
-app.listen(3000);
+app.get("/home.hbs", function(request, response){
+
+    response.render("home.hbs");
+});
+app.get("/kontakt.hbs", function(request, response){
+
+    response.render("kontakt.hbs");
+});
+
+app.get("/ueberuns.hbs", function(request, response){
+
+    response.render("ueberuns.hbs");
+});
+
+
+
+const server = app.listen(80, function () {
+    console.log('listening on port', server.address().port);
+});
