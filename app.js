d41222dc2750a542045dfcae83955761e5bf3e5f
larbito31 <boukhobza.larbi31@gmail.com>
2021-01-10 16:13:23 +0100

    online Bestellung

--- app.js
+++ app.js
@@ -49,6 +49,33 @@ app.get("/ueberuns.hbs", function(request, response){
     response.render("ueberuns.hbs");
 });
 
+app.get("/konfigurator.hbs", function(request, response){
+
+    response.render("konfigurator.hbs");
+});
+
+app.get("/bestellen.hbs", function(request, response){
+
+    response.render("bestellen.hbs");
+});
+
+app.get("/bestellen2.hbs", function(request, response){
+
+    response.render("bestellen2.hbs");
+});
+
+app.get("/bestellenKarte.hbs", function(request, response){
+
+    response.render("bestellenKarte.hbs");
+});
+
+app.get("/bestellenPizza.hbs", function(request, response){
+
+    response.render("bestellenPizza.hbs");
+});
+
+
+
 const server = app.listen(80, function () {
     console.log('listening on port', server.address().port);
 });
